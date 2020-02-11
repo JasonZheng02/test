84b7e3e9e295e238a10e0b1306fe20e6f6ba1998
Jason Zheng <jzheng02@stuy.edu>
2020-02-11 04:36:53 -0500

    .

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from draw import *
 s = new_screen()
 c = [ 0, 255, 0 ]
 
-def square(x,y,length):
+def square(x,y,length,c):
     draw_line(x, y, x + length, y, s, c)
     draw_line(x, y - length, x + length, y - length, s, c)
     draw_line(x, y - length, x, y, s, c)
     draw_line(x + length, y - length, x + length, y, s, c);
 
 for x in range(0,100):
-    square(random.randint(-490,490), random.randint(-490,490), random.randint(1,300))
+    c = [ random.randint(0,255),random.randint(0,255),random.randint(0,255)]
+    square(random.randint(-490,490), random.randint(-490,490), random.randint(1,300), c)
 
 
 display(s)
