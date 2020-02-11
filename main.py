560295a86272b98833ec95e3f0f420f2d2ab44a9
Jason Zheng <jzheng02@stuy.edu>
2020-02-11 04:30:38 -0500

    .

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def square(x,y,length):
     draw_line(x, y, x + length, y, s, c)
     draw_line(x, y - length, x + length, y - length, s, c)
     draw_line(x, y - length, x, y, s, c)
-    draw_line(0, YRES/2, XRES-1, YRES/2, s, c)
+    draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
 square(300,300,100)
 
 
