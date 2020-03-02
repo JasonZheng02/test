7687231a95965ec47af5b274ea881f896d9f6baf
JasonZheng02 <heydude506@gmail.com>
2020-03-01 23:20:21 -0500

    .

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ screen = new_screen()
 color = [ 0, 255, 0 ]
 edges = []
 transform = new_matrix()
-
 for x in range(100):
     add_edge(edges, 0, 0 + x, 0, 100, 0 + x, 0)
     add_edge(edges, 100, 0 + x, 0, 100, 0 + x, 100)
+    add_edge(edges, 0, 0, 0 + x, 100, 0, 0 + x)
+    draw_lines(edges, screen, color)
 parse_file( 'script', edges, transform, screen, color )
